634b591697fdba93447993c96138b5e57493a0aa
Harshit Karwal <karwalharshit@gmail.com>
2023-02-14 19:25:43 +0530

    49 marks w/ backend

---
+++ README.md
@@ -0,0 +1,17 @@
+# GREDDIIT
+
+Greddiit is functionally designed to be a clone of Reddit. 
+
+The app can be run by following these steps: 
+
+- Run `npm start` in the `frontend` folder.
+- Navigate to `localhost:3000` in a web browser.
+
+------
+
+The app has also been hosted on Netlify, and can be accessed by visiting `greddiit-harshk23.netlify.app`
+
+# NOTES
+
+mod role - email/mysubgreddiits/gr/name
+normal role - email/subgreddiits/gr/name
---
+++ backend/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+const usersRouter = require('./controllers/users')
+const subgreddiitsRouter = require('./controllers/subgreddiits')
+const reportsRouter = require('./controllers/reports')
+const postsRouter = require('./controllers/posts')
+const loginRouter = require('./controllers/login')
+
+const app = express()
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json({limit: '50mb'}));
+
+app.use('/api/users', usersRouter)
+app.use('/api/subgreddiits', subgreddiitsRouter)
+app.use('/api/reports', reportsRouter)
+app.use('/api/posts', postsRouter)
+app.use('/api/login', loginRouter)
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name == 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name == 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+{}
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
